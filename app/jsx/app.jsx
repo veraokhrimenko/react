232e13735ddfac271e20d2a2616b71ee7e65720a
vokhrimenko <vokhrimenko@cogniance.com>
2014-12-14 20:36:10 +0200

    add main marckup

--- app/jsx/app.jsx
+++ app/jsx/app.jsx
@@ -1,20 +1,54 @@
 /** @jsx React.DOM */
 'use strict';
-var HelloWorld = React.createClass({
+
+var Profile = React.createClass({
 	getInitialState: function() {
-		return {message: 'Hello World!'};
+		return { status: 'online', name: 'Vera Okhrimenko' };
 	},
-	goodbye: function(event) {
-		this.setState({message: 'Goodbye World.'});
-	},
+
 	render: function() {
 		return (
-			<h1 onClick={this.goodbye}>{this.state.message}</h1>
+			<div>
+				<div className="name">{this.state.name}</div>
+				<div className="status">{this.state.status}</div>
+			</div>
 		);
 	}
 });
 
+var UserList = React.createClass({
+	render: function() {
+		return (
+			<div>
+                <div>{ this.props.items.map(function(item, index){
+                    return <div className="user-item">{item.name} {item.status} </div>;
+                	}) }
+                        
+                </div>
+            </div>
+        );
+	}
+});
+
+var MessageInput = React.createClass({
+	render: function() {
+		return (
+			<input type="text" placeholder="Type your message" />
+        );
+	}
+});
+
 React.renderComponent(
-	<HelloWorld />,
-	document.getElementById('app')
+	<Profile />,
+	document.getElementById('profile')
+);
+
+React.renderComponent(
+	<MessageInput />,
+	document.getElementById('message')
+);
+
+React.renderComponent(
+    <UserList items={ [ {name: 'Home', status: 'online'}, {name: 'Services', status: 'offline'}, {name: 'About', status: 'away' }] } />,
+    document.getElementById('list')
 );
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,20 +1,54 @@
 /** @jsx React.DOM */
 'use strict';
-var HelloWorld = React.createClass({displayName: 'HelloWorld',
+
+var Profile = React.createClass({displayName: 'Profile',
 	getInitialState: function() {
-		return {message: 'Hello World!'};
+		return { status: 'online', name: 'Vera Okhrimenko' };
 	},
-	goodbye: function(event) {
-		this.setState({message: 'Goodbye World.'});
-	},
+
 	render: function() {
 		return (
-			React.DOM.h1( {onClick:this.goodbye}, this.state.message)
+			React.DOM.div(null, 
+				React.DOM.div( {className:"name"}, this.state.name),
+				React.DOM.div( {className:"status"}, this.state.status)
+			)
 		);
 	}
 });
 
+var UserList = React.createClass({displayName: 'UserList',
+	render: function() {
+		return (
+			React.DOM.div(null, 
+                React.DOM.div(null,  this.props.items.map(function(item, index){
+                    return React.DOM.div( {className:"user-item"}, item.name, item.status );
+                	}) 
+                        
+                )
+            )
+        );
+	}
+});
+
+var MessageInput = React.createClass({displayName: 'MessageInput',
+	render: function() {
+		return (
+			React.DOM.input( {type:"text", placeholder:"Type your message"} )
+        );
+	}
+});
+
 React.renderComponent(
-	HelloWorld(null ),
-	document.getElementById('app')
+	Profile(null ),
+	document.getElementById('profile')
+);
+
+React.renderComponent(
+	MessageInput(null ),
+	document.getElementById('message')
+);
+
+React.renderComponent(
+    UserList( {items: [ {name: 'Home', status: 'online'}, {name: 'Services', status: 'offline'}, {name: 'About', status: 'away' }] } ),
+    document.getElementById('list')
 );
