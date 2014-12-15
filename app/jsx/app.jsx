1c1ab1cbc371cfa470d5b3d8491f24c0e7d109f2
vokhrimenko <vokhrimenko@cogniance.com>
2014-12-15 11:24:30 +0200

    Add search for users list

--- app/jsx/app.jsx
+++ app/jsx/app.jsx
@@ -2,53 +2,90 @@
 'use strict';
 
 var Profile = React.createClass({
-	getInitialState: function() {
-		return { status: 'online', name: 'Vera Okhrimenko' };
-	},
+    getInitialState: function() {
+        return { status: 'online', name: 'Vera Okhrimenko' };
+    },
 
-	render: function() {
-		return (
-			<div>
-				<div className="name">{this.state.name}</div>
-				<div className="status">{this.state.status}</div>
-			</div>
-		);
-	}
+    render: function() {
+        return (
+            <div>
+                <div className="name">{this.state.name}</div>
+                <div className="status">{this.state.status}</div>
+            </div>
+        );
+    }
 });
 
 var UserList = React.createClass({
-	render: function() {
-		return (
-			<div>
+    render: function() {
+        return (
+            <div>
                 <div>{ this.props.items.map(function(item, index){
-                    return <div className="user-item">{item.name} {item.status} </div>;
-                	}) }
+                    return <div className="user-item">
+                            <div className="name">{item.name}</div>
+                            <div className="status">{item.status}</div>
+                        </div>
+                    }) }
                         
                 </div>
             </div>
         );
-	}
+    }
 });
 
 var MessageInput = React.createClass({
-	render: function() {
-		return (
-			<input type="text" placeholder="Type your message" />
+    render: function() {
+        return (
+            <div>
+            <input type="text" placeholder="Type your message" />
+            </div>
         );
-	}
+    }
 });
 
+var SearchInput = React.createClass({
+    getInitialState: function(){
+        return { searchString: '' };
+    },
+    handleChange: function(e) {
+        this.setState({searchString:e.target.value});
+    },
+    render: function() {
+        var libraries = this.props.items,
+            searchString = this.state.searchString.trim().toLowerCase();
+
+        if(searchString.length > 0){
+            libraries = libraries.filter(function(l){
+                return l.name.toLowerCase().match( searchString );
+            });
+        }
+
+        return (
+            <div>
+                <input type="text" value={this.state.searchString} placeholder="Search" onChange={this.handleChange} />
+                <UserList  items= { libraries }/>
+            </div>
+        );
+    }
+});
+
+var libraries = [ 
+    {name: 'User First', status: 'online', id: '1'}, 
+    {name: 'Second User', status: 'offline', id: '2'}, 
+    {name: 'One More User', status: 'away', id: '3' }
+];
+
 React.renderComponent(
-	<Profile />,
-	document.getElementById('profile')
+    <Profile />,
+    document.getElementById('profile')
 );
 
 React.renderComponent(
-	<MessageInput />,
-	document.getElementById('message')
+    <MessageInput />,
+    document.getElementById('message')
 );
 
 React.renderComponent(
-    <UserList items={ [ {name: 'Home', status: 'online'}, {name: 'Services', status: 'offline'}, {name: 'About', status: 'away' }] } />,
-    document.getElementById('list')
+    <SearchInput items={ libraries } />,
+    document.getElementById('search')
 );
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,53 +2,90 @@
 'use strict';
 
 var Profile = React.createClass({displayName: 'Profile',
-	getInitialState: function() {
-		return { status: 'online', name: 'Vera Okhrimenko' };
-	},
+    getInitialState: function() {
+        return { status: 'online', name: 'Vera Okhrimenko' };
+    },
 
-	render: function() {
-		return (
-			React.DOM.div(null, 
-				React.DOM.div( {className:"name"}, this.state.name),
-				React.DOM.div( {className:"status"}, this.state.status)
-			)
-		);
-	}
+    render: function() {
+        return (
+            React.DOM.div(null, 
+                React.DOM.div( {className:"name"}, this.state.name),
+                React.DOM.div( {className:"status"}, this.state.status)
+            )
+        );
+    }
 });
 
 var UserList = React.createClass({displayName: 'UserList',
-	render: function() {
-		return (
-			React.DOM.div(null, 
+    render: function() {
+        return (
+            React.DOM.div(null, 
                 React.DOM.div(null,  this.props.items.map(function(item, index){
-                    return React.DOM.div( {className:"user-item"}, item.name, item.status );
-                	}) 
+                    return React.DOM.div( {className:"user-item"}, 
+                            React.DOM.div( {className:"name"}, item.name),
+                            React.DOM.div( {className:"status"}, item.status)
+                        )
+                    }) 
                         
                 )
             )
         );
-	}
+    }
 });
 
 var MessageInput = React.createClass({displayName: 'MessageInput',
-	render: function() {
-		return (
-			React.DOM.input( {type:"text", placeholder:"Type your message"} )
+    render: function() {
+        return (
+            React.DOM.div(null, 
+            React.DOM.input( {type:"text", placeholder:"Type your message"} )
+            )
         );
-	}
+    }
 });
 
+var SearchInput = React.createClass({displayName: 'SearchInput',
+    getInitialState: function(){
+        return { searchString: '' };
+    },
+    handleChange: function(e) {
+        this.setState({searchString:e.target.value});
+    },
+    render: function() {
+        var libraries = this.props.items,
+            searchString = this.state.searchString.trim().toLowerCase();
+
+        if(searchString.length > 0){
+            libraries = libraries.filter(function(l){
+                return l.name.toLowerCase().match( searchString );
+            });
+        }
+
+        return (
+            React.DOM.div(null, 
+                React.DOM.input( {type:"text", value:this.state.searchString, placeholder:"Search", onChange:this.handleChange} ),
+                UserList(  {items:  libraries })
+            )
+        );
+    }
+});
+
+var libraries = [ 
+    {name: 'User First', status: 'online', id: '1'}, 
+    {name: 'Second User', status: 'offline', id: '2'}, 
+    {name: 'One More User', status: 'away', id: '3' }
+];
+
 React.renderComponent(
-	Profile(null ),
-	document.getElementById('profile')
+    Profile(null ),
+    document.getElementById('profile')
 );
 
 React.renderComponent(
-	MessageInput(null ),
-	document.getElementById('message')
+    MessageInput(null ),
+    document.getElementById('message')
 );
 
 React.renderComponent(
-    UserList( {items: [ {name: 'Home', status: 'online'}, {name: 'Services', status: 'offline'}, {name: 'About', status: 'away' }] } ),
-    document.getElementById('list')
+    SearchInput( {items: libraries } ),
+    document.getElementById('search')
 );
